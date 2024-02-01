cfec5757ae49399775c8b2747d9c0761ceaa1bd2
SandeepSingh <100891329+sanizuku@users.noreply.github.com>
2024-02-01 18:25:07 +0530

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,15 @@
+let country="India";
+let continent="Asia";
+let population=140
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Datatype
+let isisland=false;
+let language;
+
+console.log(typeof isisland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language)
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,67 @@
+/*let js="amazing";//values are smallest unit in a form of information that we have in javascript
+if(js==="amazing")
+alert("Javascript is Fun");
+40+8+23-10;
+console.log(40+8+23-10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName="Ajay";//generally we use camelCase for variable name 
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//some convention while chosing variable name\
+ let ajay_Pathak="KL";//can use _
+ let $function=27;
+ let name="Jonas"; //illegal to use but it works ----some reserved keyword =new,function ...etc
+ let Person ="jonas"; //variable name start with upper case = should not use though it is not illegal but specific use case =oops
+ let person="jonas";
+
+ let PI = 3.1415; //real constant should be in uppercase
+ let myFirstJob="Programmer";//make sure variable name should be descriptive
+ let myCurrentJob="Teacher";//so this approach is lot better
+
+//  let job1="programmer"; not descriptive
+//  let job2="teacher";
+console.log(myFirstJob)
+*/
+
+
+
+//Data Types 7(number,string,boolean,Undefined,Null,Symbol(ES2015 or Es6),BigInt(ES2020) Top to bottom Execution
+//Dynamic type i.e value has datatype not variable
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof true );
+
+console.log(typeof javaScriptIsFun);
+
+console.log(typeof 23);
+console.log(typeof "jonas");
+
+javaScriptIsFun="YES";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);//here value is undefined and typeofv ariable is also undefined
+
+year=1991;
+console.log(typeof year);
+
+
+//ERROR
+console.log(typeof null); // console type of null is object but it is an error due to legacy reason but actually type of null is null
+
+
+
+
+
+
+
+
+
